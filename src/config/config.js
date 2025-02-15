fab7cb8c2deab4003b3a413ccf6c816531985bf2
Sergio Gutierrez Fernandez <147081722+serguccierrez@users.noreply.github.com>
2025-02-15 10:57:23 +0100

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  server_url: "http://api.weatherapi.com/v1/forecast.json",
+  api_key: "<INTRODUCIR API_KEY DE OPENWEATHERMAP>, (https://www.weatherapi.com/)",
+  num_items_query: 9,
+  num_items_show: 8,
+  default_lat: 40.416775,
+  default_lon: -3.703790,
+  use_server: true,
+  force_error: false  
+}
+
+
+export default CONFIG;
